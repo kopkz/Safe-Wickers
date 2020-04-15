6a2fa232bc912d7680bb2cc104676f9dc1dbde42
kopkz <kopkz1994@gmail.com>
2020-04-15 13:52:35 +1000

    initial project

---
+++ Safe Wickers/Safe Wickers/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Safe Wickers
+//
+//  Created by 匡正 on 15/4/20.
+//  Copyright © 2020 匡正. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
